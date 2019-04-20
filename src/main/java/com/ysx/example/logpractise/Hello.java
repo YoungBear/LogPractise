02000b2fe8fc313c67ba99ef19324325a738e75e
youngbear <youngbear@aliyun.com>
2019-04-21 07:36:59 +0800

    init repository
    Signed-off-by: youngbear <youngbear@aliyun.com>

---
+++ src/main/java/com/ysx/example/logpractise/Hello.java
@@ -0,0 +1,15 @@
+package com.ysx.example.logpractise;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2019-04-21 07:36
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Logs");
+    }
+}
